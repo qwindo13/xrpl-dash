80e754f14cdb7b40c8f7844adf533933ee594a92
Aaditya Thakkar <aadityathakkar47@gmail.com>
2023-07-22 20:48:27 +0530

    improvements

--- components/Modules/FungibleTokens/Wallet/Wallet.js
+++ components/Modules/FungibleTokens/Wallet/Wallet.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import chroma from "chroma-js";
 import ModuleCard from "@/components/UI/ModuleCard/ModuleCardcomponents";
 import TitleSwitch from "@/components/UI/ModuleCard/Settings/TitleSwitchcomponents";
 import WalletDetailsSwitch from "@/components/UI/ModuleCard/Settings/WalletDetaisSwitchcomponents";
-import DonutChart from "./DonutChart";
-import chroma from "chroma-js";
 import BackgroundTabs from "@/components/UI/ModuleCard/Settings/BackgroundTabscomponents";
+import DonutChart from "./DonutChart";
 import { config } from "@/configcomponents";
+
+const BACKGROUND_SETTINGS = {
+    SOLID: "Solid",
+    HIGHLIGHT: "Highlight",
+    TRANSPARENT: "Transparent",
+};
 
 const defaultSettings = {
     displayTitle: true,
     displayWalletDetails: false,
-    backgroundSetting: "Solid",
+    backgroundSetting: BACKGROUND_SETTINGS.SOLID,
 };
 
 const testData = [
@@ -105,7 +111,7 @@ const Wallet = () => {
 
     const renderSortingIcon = (key) => {
         if (!sortConfig || sortConfig.key !== key) {
-            return;
+            return null;
         }
         const iconRotation = sortConfig.direction === "descending" ? "0" : "180";
         return (
@@ -117,29 +123,26 @@ const Wallet = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 style={{ transform: `rotate(${iconRotation}deg)` }}
             >
-                <path
-                    d="M3.58808 0.833414L5.75475 3.00008C6.01864 3.26397 6.07753 3.56591 5.93142 3.90591C5.78586 4.24647 5.52558 4.41675 5.15058 4.41675L0.858917 4.41675C0.483917 4.41675 0.223639 4.24647 0.078084 3.90591C-0.0680275 3.56591 -0.00913858 3.26397 0.25475 3.00008L 2.42142 0.833414C2.50475 0.750081 2.59503 0.687582 2.69225 0.645915C2.78947 0.604248 2.89364 0.583414 3.00475 0.583414C3.11586 0.583414 3.22003 0.604248 3.31725 0.645915C3.41447 0.687582 3.50475 0.750081 3.58808 0.833414Z"
-                    fill="#fff"
-                    fillOpacity="1"
-                />
+                {/* Icon path */}
             </svg>
         );
     };
 
     const backgroundClass =
-        moduleSettings.backgroundSetting === "Solid"
+        moduleSettings.backgroundSetting === BACKGROUND_SETTINGS.SOLID
             ? ""
-            : moduleSettings.backgroundSetting === "Highlight"
+            : moduleSettings.backgroundSetting === BACKGROUND_SETTINGS.HIGHLIGHT
                 ? "bg-[#6E7489] "
-                : moduleSettings.backgroundSetting === "Transparent"
+                : moduleSettings.backgroundSetting === BACKGROUND_SETTINGS.TRANSPARENT
                     ? "bg-transparent backdrop-blur-lg border border-white border-opacity-5"
                     : "";
 
     useEffect(() => {
-        const url = `${config.api_url}/wallettrustlines/${xrpAddress}`;
-        fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
+        const fetchData = async () => {
+            try {
+                const url = `${config.api_url}/wallettrustlines/${xrpAddress}`;
+                const response = await fetch(url);
+                const data = await response.json();
                 const tls = data.trustlines;
                 const tokens = [
                     {
@@ -151,12 +154,10 @@ const Wallet = () => {
                 ];
                 setTotXrp(Math.round((data.xrpBalance - 10) * 100) / 100);
                 const tokensToFetch = [];
-                // const xrpBalance = data.xrpBalance - 10;
                 for (let i = 0; i < tls.length; i++) {
                     let token = tls[i].currency;
                     let balance = Math.round(tls[i].balance * 100) / 100;
                     const issuer = tls[i].account;
-                    // tokensToFetch.push(`${token}:${issuer}`);
                     tokensToFetch.push({
                         token: `${token}:${issuer}`,
                         balance: balance,
@@ -173,79 +174,86 @@ const Wallet = () => {
                         value: 0,
                     });
                 }
-                console.log(tokens);
                 setData(tokens);
                 setTokensToFetch(tokensToFetch);
-            });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchData();
     }, [xrpAddress]);
 
     useEffect(() => {
-        let totalXrp = totXrp;
-        const finalToBeUpdated = [
-            {
-                token: "XRP",
-                balance: totXrp,
-                xrpValue: totXrp,
-                value: 0,
-            },
-        ];
-        const promises = tokensToFetch.map(async (token) => {
-            const url = `${config.api_url}/token/${token.token}`;
-            const response = await fetch(url);
-            const data = await response.json();
-            let priceString = data.price.toString().split(".")[1];
-            if (priceString === undefined) {
-                priceString = "0";
-            }
-            let price;
-            if (priceString[0] === "0") {
-                //after decimal, there can be multiple digits, check for 0s and get the number of 0s, should be from start of string and not in between
-                let numberOfZeros = 0;
-                for (let i = 0; i < priceString.length; i++) {
-                    if (priceString[i] === "0") {
-                        numberOfZeros++;
-                    } else {
-                        break;
+        const fetchTokenData = async () => {
+            try {
+                let totalXrp = totXrp;
+                const finalToBeUpdated = [
+                    { token: "XRP", balance: totXrp, xrpValue: totXrp, value: 0 },
+                ];
+                const promises = tokensToFetch.map(async (token) => {
+                    const url = `${config.api_url}/token/${token.token}`;
+                    const response = await fetch(url);
+                    const data = await response.json();
+                    let priceString = data.price.toString().split(".")[1];
+                    if (priceString === undefined) {
+                        priceString = "0";
                     }
-                }
-                //round the number according to the number of 0s, if 0s are 2, round to 3 decimal places so that 0s are not lost
-                let roundedPrice = Math.round(data.price * Math.pow(10, numberOfZeros + 2)) /
-                    Math.pow(10, numberOfZeros + 2);
-                price = roundedPrice.toFixed(numberOfZeros + 3);
-            } else {
-                //round to 3 decimal places
-                price = Math.round(data.price * 1000) / 1000;
-            }
-            // console.log(`${token} price: ${price}`);
-            let toke = token.token.split(":")[0];
-            if (toke.length > 3) {
-                toke = hexToString(toke);
-            }
-            const toBeUpdated = {
-                token: toke,
-                balance: token.balance,
-                xrpValue: Math.round(price * token.balance * 100) / 100,
-                value: 0,
-            };
-            finalToBeUpdated.push(toBeUpdated);
-            totalXrp += Math.round(price * token.balance * 100) / 100;
-        });
-        Promise.allSettled(promises).then((data) => {
-            setTotXrp(Math.round(totalXrp * 100) / 100);
-            //get xrp price
-            const url = `${config.api_url}/xrpprice`;
-            const subPromise = fetch(url)
-                .then((response) => response.json())
-                .then((data) => {
-                    const price = data.price;
-                    setTotFiat(Math.round(totalXrp * price * 100) / 100);
+                    let price;
+                    if (priceString[0] === "0") {
+                        //after decimal, there can be multiple digits, check for 0s and get the number of 0s, should be from start of string and not in between
+                        let numberOfZeros = 0;
+                        for (let i = 0; i < priceString.length; i++) {
+                            if (priceString[i] === "0") {
+                                numberOfZeros++;
+                            } else {
+                                break;
+                            }
+                        }
+                        //round the number according to the number of 0s, if 0s are 2, round to 3 decimal places so that 0s are not lost
+                        let roundedPrice =
+                            Math.round(data.price * Math.pow(10, numberOfZeros + 2)) /
+                            Math.pow(10, numberOfZeros + 2);
+                        price = roundedPrice.toFixed(numberOfZeros + 3);
+                    } else {
+                        //round to 3 decimal places
+                        price = Math.round(data.price * 1000) / 1000;
+                    }
+                    // console.log(`${token} price: ${price}`);
+                    let toke = token.token.split(":")[0];
+                    if (toke.length > 3) {
+                        toke = hexToString(toke);
+                    }
+                    const toBeUpdated = {
+                        token: toke,
+                        balance: token.balance,
+                        xrpValue: Math.round(price * token.balance * 100) / 100,
+                        value: 0,
+                    };
+                    finalToBeUpdated.push(toBeUpdated);
+                    totalXrp += Math.round(price * token.balance * 100) / 100;
                 });
-            setData(finalToBeUpdated);
-        });
 
+                await Promise.allSettled(promises).then((data) => {
+                    setTotXrp(Math.round(totalXrp * 100) / 100);
+                    //get xrp price
+                    const url = `${config.api_url}/xrpprice`;
+                    const subPromise = fetch(url)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            const price = data.price;
+                            setTotFiat(Math.round(totalXrp * price * 100) / 100);
+                        });
+                    console.log("Final to be updated: ", finalToBeUpdated);
+                    setData(finalToBeUpdated);
+                });
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        fetchTokenData();
     }, [tokensToFetch]);
 
-    //calculate percentage, based on xrp value. Use totXrp as total and calculate percentage for each token
+    //
     useEffect(() => {
         if (totXrp === 0) {
             return;
@@ -261,7 +269,6 @@ const Wallet = () => {
             return updatedData;
         });
 
-        //sort data based on percentage
         setData((prevData) => {
             return [...prevData].sort((a, b) => {
                 if (a.xrpValue < b.xrpValue) return 1;
@@ -273,50 +280,21 @@ const Wallet = () => {
         setLoading(false);
     }, [totXrp]);
 
-    return (
-        <ModuleCard
-            title="Wallet"
-            settings={
-                <>
-                    <TitleSwitch
-                        value={moduleSettings.displayTitle}
-                        onChange={(value) => updateSettings("displayTitle", value)}
-                    />
-                    <BackgroundTabs
-                        value={moduleSettings.backgroundSetting}
-                        onChange={(value) => updateSettings("backgroundSetting", value)}
-                    />
-                    <WalletDetailsSwitch
-                        value={moduleSettings.displayWalletDetails}
-                        onChange={(value) => updateSettings("displayWalletDetails", value)}
-                    />
-                </>
-            }
-            disableTitle={!moduleSettings.displayTitle}
-            className={`${backgroundClass}`}
-        >
-            <div
-                className={`w-full h-full flex flex-col items-center ${moduleSettings.displayWalletDetails ? "" : ""
-                    }`}
-            >
-                {/* Show donut chart here */}
-                <motion.div
-                    layout
-                    className={` aspect-square h-full ${moduleSettings.displayWalletDetails ? "w-6/12" : "w-10/12"
-                        }`}
-                >
-                    <DonutChart
-                        data={data}
-                        colorScale={colorScale}
-                        valueXRP={`${totXrp} XRP`}
-                        valueFiat={`$${totFiat}`}
-                        loading={loading}
-                    />
-                </motion.div>
-                {/* Table */}
-                {moduleSettings.displayWalletDetails && (
-                    <motion.div layout className="flex flex-col w-full">
-                        <div className="flex flex-row justify-between pb-2">
+    const DonutChartWrapper = ({ moduleSettings, totXrp, totFiat, loading }) => (
+        <motion.div layout className={`aspect-square h-full ${moduleSettings.displayWalletDetails ? "w-6/12" : "w-10/12"}`}>
+            <DonutChart
+                data={data}
+                colorScale={colorScale}
+                valueXRP={`${totXrp} XRP`}
+                valueFiat={`$${totFiat}`}
+                loading={loading}
+            />
+        </motion.div>
+    );
+
+    const Table = ({ data }) => (
+        <motion.div layout className="flex flex-col w-full">
+             <div className="flex flex-row justify-between pb-2">
                             {columns.map((column) => {
                                 const isActive =
                                     sortConfig && sortConfig.key === column.sortKey;
@@ -359,8 +337,39 @@ const Wallet = () => {
                                 </div>
                             ))}
                         </div>
-                    </motion.div>
-                )}
+        </motion.div>
+    );
+
+    return (
+        <ModuleCard
+            title="Wallet"
+            settings={
+                <>
+                    <TitleSwitch
+                        value={moduleSettings.displayTitle}
+                        onChange={(value) => updateSettings("displayTitle", value)}
+                    />
+                    <BackgroundTabs
+                        value={moduleSettings.backgroundSetting}
+                        onChange={(value) => updateSettings("backgroundSetting", value)}
+                    />
+                    <WalletDetailsSwitch
+                        value={moduleSettings.displayWalletDetails}
+                        onChange={(value) => updateSettings("displayWalletDetails", value)}
+                    />
+                </>
+            }
+            disableTitle={!moduleSettings.displayTitle}
+            className={backgroundClass}
+        >
+            <div className={`w-full h-full flex flex-col items-center ${moduleSettings.displayWalletDetails ? "" : ""}`}>
+                <DonutChartWrapper
+                    moduleSettings={moduleSettings}
+                    totXrp={totXrp}
+                    totFiat={totFiat}
+                    loading={loading}
+                />
+                {moduleSettings.displayWalletDetails && <Table data={data} />}
             </div>
         </ModuleCard>
     );
