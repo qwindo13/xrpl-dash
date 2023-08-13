ce195858dc646671a049619bace6654312cdae98
qwindo13 <qwindo13@gmail.com>
2023-08-13 22:06:21 +0100

    Minor Visual Improvements

--- components/Header/SideMenu.js
+++ components/Header/SideMenu.js
@@ -7,16 +7,16 @@ import Modal from "../UI/Modal/Modal";
 import { useBalance } from '@/hooks/useBalance';
 
 function formatNumber(num) {
-  return new Intl.NumberFormat('en-US').format(num);
+    return new Intl.NumberFormat('en-US').format(num);
 }
 
 function SideMenu({ openModal, xrpAddress, truncateAddress, showModal, closeModal, className }) {
     const [isLoading, setIsLoading] = useState(true);
-    const {xrpbalance: balance} = useBalance()
- 
+    const { xrpbalance: balance } = useBalance()
+
     return (
         <Modal showModal={showModal} closeModal={closeModal} className={className}>
-            <div className="flex flex-col md:flex-col justify-between gap-8">
+            <div className="flex flex-col md:flex-col justify-between gap-8 h-full">
                 <div className="w-full flex flex-row justify-between items-center">
                     <Button onClick={openModal} className="!px-0 text-2xl bg-transparent font-semibold" disableAnimation>
                         <div className="rounded-full h-12 w-12 mr-4 bg-default-avatar" title={xrpAddress}></div>
@@ -26,7 +26,7 @@ function SideMenu({ openModal, xrpAddress, truncateAddress, showModal, closeModa
                                 <span className="text-base font-semibold opacity-60">{truncateAddress(xrpAddress)}</span>
                             </Tooltip>
                         </div>
-                        
+
                     </Button>
                     <Button
                         onClick={() => {
@@ -37,18 +37,27 @@ function SideMenu({ openModal, xrpAddress, truncateAddress, showModal, closeModa
                         <LogoutRoundedIcon />
                     </Button>
                 </div>
-                <div className="flex flex-col">
-                    <span className="text-base font-semibold opacity-60">Wallet Balance</span>
-                    {isLoading ? (
-                        <span className='text-3xl font-semibold'>{formatNumber((Math.round(balance * 100) / 100) - 10)} XRP</span>
-                    ) : (
-                        <span className="h-8 animate-pulse bg-[#A6B0CF] bg-opacity-5 rounded-lg w-6/12" />
 
-                    )}
-                </div>
+                <div className='flex flex-col justify-between h-full'>
+
+                {/* MENU */}
                 <div className="flex flex-col gap-4">
                     <Link href="/profile"><span className="text-2xl font-semibold">My profile</span></Link>
                     <Link href="/settings/profile"><span className="text-2xl font-semibold">Settings</span></Link>
+                </div>
+
+                {/* WALLET DETAILS */}
+                <div className='p-4 border border-white border-opacity-5 rounded-xl'>
+                    <div className="flex flex-col">
+                        <span className="text-sm font-semibold opacity-60">Wallet Balance</span>
+                        {isLoading ? (
+                            <span className='text-xl font-semibold'>{formatNumber((Math.round(balance * 100) / 100) - 10)} XRP</span>
+                        ) : (
+                            <span className="h-8 animate-pulse bg-[#A6B0CF] bg-opacity-5 rounded-lg w-6/12" />
+
+                        )}
+                    </div>
+                </div>
                 </div>
             </div>
 
--- components/Layouts/SettingsLayout.js
+++ components/Layouts/SettingsLayout.js
@@ -11,9 +11,11 @@ function SettingsLayout({ children }) {
                 <div className="flex flex-start">
                     <h2 className="text-4xl font-semibold">Settings</h2>
                 </div>
-                <div className="w-full flex flex-col lg:flex-row gap-16">
-                    <nav className="w-64 lg:min-h-screen flex flex-row lg:flex-col gap-8 ">
-                        <ul className="flex  flex-row lg:flex-col gap-4">
+                <div className="w-full flex flex-col lg:flex-row gap-16 relative h-full">
+
+                    {/* SIDEMENU */}
+                    <nav className="w-64 lg:min-h-screen flex flex-row lg:flex-col gap-8  ">
+                        <ul className="flex  flex-row lg:flex-col gap-4 sticky top-[134px]">
                             {[
                                 { name: 'Profile', path: '/settings/profile' },
                                 { name: 'Alerts', path: '/settings/alerts' },
@@ -32,14 +34,18 @@ function SettingsLayout({ children }) {
                             ))}
                         </ul>
                     </nav>
+
+                    {/* CONTENT */}
                     <div className="flex-grow">
                         {children}
                     </div>
+
+                    {/* CALL TO ACTION */}
                     <div className="w-64">
-                        <div className="w-full bg-white rounded-2xl p-4">
-                            <h2 className="text-2xl font-semibold text-black">Become Premium</h2>
-                            <h2 className="text-2xl font-semibold text-black">Call to action</h2>
-        
+                        <div className="w-full bg-[#21212A] rounded-2xl p-4 sticky top-[134px]">
+                            <h2 className="text-2xl font-semibold mb-4">Upgrade to Premium</h2>
+                            <p className="mb-6 opacity-60">Experience the full power of XRPLDash with the premium version. Unlock all features now!</p>
+                            <Button className="bg-white !text-[#1A1921]">Upgrade Now</Button>
                         </div>
                     </div>
 
