2fc68f97b652428b6ea89cd5d1b6619fd208cda3
qwindo13 <qwindo13@gmail.com>
2023-09-10 21:09:07 +0100

    Added select nft modal

--- components/Modules/NonFungibleTokens/SingleNft/SingleNft.js
+++ components/Modules/NonFungibleTokens/SingleNft/SingleNft.js
@@ -7,7 +7,12 @@ import SearchNftButton from "@/components/UI/ModuleCard/Settings/SearchNftButton
 import RandomSwitch from "@/components/UI/ModuleCard/Settings/RandomSwitchcomponents";
 import BackgroundTabs from "@/components/UI/ModuleCard/Settings/BackgroundTabscomponents";
 import Nft from "@/components/UI/Nft/Nftcomponents";
+import Modal from "@/components/UI/Modal/Modalcomponents";
+import Button from "@/components/UI/Button/Buttoncomponents";
 import WalletPrompt from "@/components/UI/WalletPrompt/WalletPromptcomponents";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import SliderButton from "@/components/UI/SliderButton/SliderButtoncomponents";
 
 const defaultSettings = {
     RandomSwitch: true,
@@ -16,7 +21,28 @@ const defaultSettings = {
     backgroundSetting: "Solid",
 };
 
+
+
 const SingleNft = ({ data, index }) => {
+
+    const [showModal, setShowModal] = useState(false);
+    const closeModal = () => setShowModal(false);
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [loaded, setLoaded] = useState(false);
+
+    const [sliderRef, instanceRef] = useKeenSlider({
+        mode: "snap",
+        rtl: false,
+        slides: { perView: "auto", spacing: 16 },
+        initial: 0,
+        slideChanged(slider) {
+            setCurrentSlide(slider.track.details.rel);
+        },
+        created() {
+            setLoaded(true);
+        },
+    });
 
     const [moduleSettings, setModuleSettings] = useState(defaultSettings);
     const updateSettings = (key, value) => {
@@ -33,42 +59,38 @@ const SingleNft = ({ data, index }) => {
         setXrpAddress(address);
     }, []);
 
+    const openModal = () => setShowModal(true);
+    const closeAvatarModal = () => setShowModal(false);
+
 
     const backgroundClass = moduleSettings.backgroundSetting === 'Solid' ? '' :
         moduleSettings.backgroundSetting === 'Highlight' ? 'bg-[#525567] ' :
             moduleSettings.backgroundSetting === 'Transparent' ? 'bg-transparent backdrop-blur-lg border border-white border-opacity-5' : '';
 
     return (
-        <ModuleCard
+        <><ModuleCard
             title="NFT - #23123"
-            settings={
-                <>
-                    <RandomSwitch
-                        value={moduleSettings.randomNFT}
-                        onChange={(value) => updateSettings("randomNFT", value)}
-                    />
-                    {!moduleSettings.randomNFT && (
-                        <SearchNftButton
-                            value={moduleSettings.backgroundSetting}
-                            onChange={(value) => updateSettings("backgroundSetting", value)}
-                        />
-                    )}
-
-                    <NftNameSwitch
-                        value={moduleSettings.displayNftName}
-                        onChange={(value) => updateSettings("displayNftName", value)}
-                    />
-                    <NftPriceSwitch
-                        value={moduleSettings.displayNftPrice}
-                        onChange={(value) => updateSettings("displayNftPrice", value)}
-                    />
-                    <BackgroundTabs
+            settings={<>
+                <RandomSwitch
+                    value={moduleSettings.randomNFT}
+                    onChange={(value) => updateSettings("randomNFT", value)} />
+                {!moduleSettings.randomNFT && (
+                    <SearchNftButton
                         value={moduleSettings.backgroundSetting}
-                        onChange={(value) => updateSettings("backgroundSetting", value)}
-                    />
+                        onChange={(value) => updateSettings("backgroundSetting", value)} />
+                )}
 
-                </>
-            }
+                <NftNameSwitch
+                    value={moduleSettings.displayNftName}
+                    onChange={(value) => updateSettings("displayNftName", value)} />
+                <NftPriceSwitch
+                    value={moduleSettings.displayNftPrice}
+                    onChange={(value) => updateSettings("displayNftPrice", value)} />
+                <BackgroundTabs
+                    value={moduleSettings.backgroundSetting}
+                    onChange={(value) => updateSettings("backgroundSetting", value)} />
+
+            </>}
             disableTitle="true"
             className={`${backgroundClass} !p-0`}
         >
@@ -79,11 +101,50 @@ const SingleNft = ({ data, index }) => {
                 className="w-full h-full border-none"
                 src='/images/nft.webp'
                 displayName={moduleSettings.displayNftName}
-                displayPrice={moduleSettings.displayNftPrice}
-            />
+                displayPrice={moduleSettings.displayNftPrice} />
 
-        </ModuleCard>
+        </ModuleCard><Modal
+            title="Change avatar"
+            description="Select an NFT from your wallet to be displayed."
+            showModal={showModal}
+            closeModal={closeModal}
+        >
+                <div className="relative mb-4 md:mb-8">
+                    <div ref={sliderRef} className="keen-slider ">
+                        {/* Put array map here */}
+                    </div>
+                    <div className="absolute top-1/2 left-0 transform -translate-y-1/2  flex flex-row justify-between">
+                        {loaded && instanceRef.current && (
+                            <>
+                                <SliderButton
+                                    left
+                                    onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()}
+                                    disabled={currentSlide === 0} />
+                            </>
+                        )}
+                    </div>
+                    <div className="absolute top-1/2 right-0 transform -translate-y-1/2  flex flex-row justify-between">
+                        {loaded && instanceRef.current && (
+                            <>
+                                <SliderButton
+                                    right
+                                    onClick={(e) => e.stopPropagation() || instanceRef.current?.next()}
+                                    disabled={currentSlide === instanceRef.current.track.details.slides.length - 3} />
+                            </>
+                        )}
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <Button className="bg-white !text-[#1A1921]" onClick={closeAvatarModal}>
+                        Select
+                    </Button>
+                </div>
+            </Modal></>
     );
 };
 
+
+
 export default SingleNft;
+
+
--- pages/settings/profile.js
+++ pages/settings/profile.js
@@ -532,77 +532,7 @@ function ProfileSettings({ children }) {
       </Modal>
 
       <TxModal showModal={showModal} closeModal={closeModal} qrCode={qrCode} qrCodeUrl={qrCodeUrl} text='verification process' />
-      {/* <Modal showModal={showModal} closeModal={closeModal}>
-        <div>
-          <Image
-            className="absolute p-0 top-[-38px] left-0"
-            src="/images/xumm-logo.svg"
-            height={130}
-            width={120}
-            alt="XUMM"
-          />
-          <div className="w-full flex flex-row justify-between items-start pb-8 relative">
-            <h3 className="font-semibold text-xl">
-              Scan the QR code with XUMM app to verify your account
-            </h3>
-            <button onClick={() => setXummButtonClicked(false)}>
-              {" "}
-              <CloseRoundedIcon />{" "}
-            </button>
-          </div>
-          <div className="flex flex-col md:flex-row justify-between gap-16 ">
-            <div className="flex flex-col gap-16 w-full md:w-1/2 justify-between">
-              <div className="flex flex-col gap-4">
-                <div className="flex items-center">
-                  <div className="w-6 h-6 flex items-center justify-center rounded-full aspect-square bg-[#1A1921]  mr-2">
-                    <span className="text-white font-bold">1</span>
-                  </div>
-                  <p>Open your XUMM app on your phone.</p>
-                </div>
-                <div className="flex items-center">
-                  <div className="w-6 h-6 flex items-center justify-center rounded-full aspect-square bg-[#1A1921]  mr-2">
-                    <span className="text-white font-bold">2</span>
-                  </div>
-                  <p>Scan the QR code below.</p>
-                </div>
-                <div className="flex items-center">
-                  <div className="w-6 h-6 flex items-center justify-center rounded-full aspect-square bg-[#1A1921] mr-2">
-                    <span className="text-white font-bold">3</span>
-                  </div>
-                  <p>
-                    Follow the instructions on your app to complete the verification process.
-                  </p>
-                </div>
-              </div>
-              <div className="hidden md:flex p-4 bg-[#272832] rounded-xl">
-                <p>
-                  Don’t see a Scan option? <br></br>Update your XUMM app to the
-                  latest version and try again.
-                </p>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2">
-              {qrCode ? (
-                <a href={qrCodeUrl} target="_blank" rel="noreferrer">
-                  <img
-                    className="w-full rounded-xl"
-                    src={qrCode}
-                    alt="XUMM QR"
-                  />
-                </a>
-              ) : (
-                <Image
-                  className="w-full blur-sm"
-                  src="/images/qr.png"
-                  height={200}
-                  width={200}
-                  alt="XUMM QR"
-                />
-              )}
-            </div>
-          </div>
-        </div>
-      </Modal> */}
+
     </>
   );
 }
