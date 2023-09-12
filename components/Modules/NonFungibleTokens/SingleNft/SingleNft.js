7a1a672cd21536079bf46b92407e7634fbf30e37
Aaditya-T <aadityathakkar47@gmail.com>
2023-09-12 17:43:03 +0530

    changes to nft module

--- components/Modules/NonFungibleTokens/SingleNft/SingleNft.js
+++ components/Modules/NonFungibleTokens/SingleNft/SingleNft.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Image from 'next/image';
-import ModuleCard from '@/components/UI/ModuleCard/ModuleCardcomponents';
+import Image from "next/image";
+import ModuleCard from "@/components/UI/ModuleCard/ModuleCardcomponents";
 import NftNameSwitch from "@/components/UI/ModuleCard/Settings/NftNameSwitchcomponents";
 import NftPriceSwitch from "@/components/UI/ModuleCard/Settings/NftPriceSwitchcomponents";
 import SearchNftButton from "@/components/UI/ModuleCard/Settings/SearchNftButtoncomponents";
@@ -15,136 +15,162 @@ import { useKeenSlider } from "keen-slider/react";
 import SliderButton from "@/components/UI/SliderButton/SliderButtoncomponents";
 
 const defaultSettings = {
-    RandomSwitch: true,
-    displayNftName: true,
-    displayNftPrice: false,
-    backgroundSetting: "Solid",
+  RandomSwitch: true,
+  displayNftName: true,
+  displayNftPrice: false,
+  backgroundSetting: "Solid",
+  nft: false
 };
-
-
 
 const SingleNft = ({ data, index }) => {
+  const [showModal, setShowModal] = useState(false);
+  const closeModal = () => setShowModal(false);
 
-    const [showModal, setShowModal] = useState(false);
-    const closeModal = () => setShowModal(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
 
-    const [currentSlide, setCurrentSlide] = useState(0);
-    const [loaded, setLoaded] = useState(false);
+  const [sliderRef, instanceRef] = useKeenSlider({
+    mode: "snap",
+    rtl: false,
+    slides: { perView: "auto", spacing: 16 },
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    },
+  });
 
-    const [sliderRef, instanceRef] = useKeenSlider({
-        mode: "snap",
-        rtl: false,
-        slides: { perView: "auto", spacing: 16 },
-        initial: 0,
-        slideChanged(slider) {
-            setCurrentSlide(slider.track.details.rel);
-        },
-        created() {
-            setLoaded(true);
-        },
-    });
+  const [moduleSettings, setModuleSettings] = useState(defaultSettings);
+  const updateSettings = (key, value) => {
+    setModuleSettings((prevSettings) => ({
+      ...prevSettings,
+      [key]: value,
+    }));
+  };
 
-    const [moduleSettings, setModuleSettings] = useState(defaultSettings);
-    const updateSettings = (key, value) => {
-        setModuleSettings((prevSettings) => ({
-            ...prevSettings,
-            [key]: value,
-        }));
-    };
+  const [xrpAddress, setXrpAddress] = useState(null);
 
-    const [xrpAddress, setXrpAddress] = useState(null);
+  useEffect(() => {
+    const address = localStorage.getItem("address");
+    setXrpAddress(address);
+  }, []);
+
+  const openModal = () => setShowModal(true);
+
+  const backgroundClass =
+    moduleSettings.backgroundSetting === "Solid"
+      ? ""
+      : moduleSettings.backgroundSetting === "Highlight"
+      ? "bg-[#525567] "
+      : moduleSettings.backgroundSetting === "Transparent"
+      ? "bg-transparent backdrop-blur-lg border border-white border-opacity-5"
+      : "";
+
 
     useEffect(() => {
-        const address = localStorage.getItem("address");
-        setXrpAddress(address);
-    }, []);
+        // console.log(moduleSettings)
+        if (moduleSettings.nft === true) {
+            openModal()
+        } else {
+            closeModal()
+        }
+    }, [moduleSettings])
+    
+  return (
+    <>
+      <ModuleCard
+        title="NFT - #23123"
+        settings={
+          <>
+            <RandomSwitch
+              value={moduleSettings.randomNFT}
+              onChange={(value) => updateSettings("randomNFT", value)}
+            />
+            {!moduleSettings.randomNFT && (
+              <SearchNftButton
+                value={moduleSettings.nft}
+                onChange={(value) => updateSettings("nft", value)}
+              />
+            )}
 
-    const openModal = () => setShowModal(true);
-    const closeAvatarModal = () => setShowModal(false);
+            <NftNameSwitch
+              value={moduleSettings.displayNftName}
+              onChange={(value) => updateSettings("displayNftName", value)}
+            />
+            <NftPriceSwitch
+              value={moduleSettings.displayNftPrice}
+              onChange={(value) => updateSettings("displayNftPrice", value)}
+            />
+            <BackgroundTabs
+              value={moduleSettings.backgroundSetting}
+              onChange={(value) => updateSettings("backgroundSetting", value)}
+            />
+          </>
+        }
+        disableTitle="true"
+        className={`${backgroundClass} !p-0`}
+      >
+        <Nft
+          imageSize="object-cover !w-full !h-full min-h-0"
+          className="w-full h-full border-none"
+          src="/images/nft.webp"
+          displayName={moduleSettings.displayNftName}
+          displayPrice={moduleSettings.displayNftPrice}
+        />
+      </ModuleCard>
 
-
-    const backgroundClass = moduleSettings.backgroundSetting === 'Solid' ? '' :
-        moduleSettings.backgroundSetting === 'Highlight' ? 'bg-[#525567] ' :
-            moduleSettings.backgroundSetting === 'Transparent' ? 'bg-transparent backdrop-blur-lg border border-white border-opacity-5' : '';
-
-    return (
-        <><ModuleCard
-            title="NFT - #23123"
-            settings={<>
-                <RandomSwitch
-                    value={moduleSettings.randomNFT}
-                    onChange={(value) => updateSettings("randomNFT", value)} />
-                {!moduleSettings.randomNFT && (
-                    <SearchNftButton
-                        value={moduleSettings.backgroundSetting}
-                        onChange={(value) => updateSettings("backgroundSetting", value)} />
-                )}
-
-                <NftNameSwitch
-                    value={moduleSettings.displayNftName}
-                    onChange={(value) => updateSettings("displayNftName", value)} />
-                <NftPriceSwitch
-                    value={moduleSettings.displayNftPrice}
-                    onChange={(value) => updateSettings("displayNftPrice", value)} />
-                <BackgroundTabs
-                    value={moduleSettings.backgroundSetting}
-                    onChange={(value) => updateSettings("backgroundSetting", value)} />
-
-            </>}
-            disableTitle="true"
-            className={`${backgroundClass} !p-0`}
-        >
-
-
-            <Nft
-                imageSize="object-cover !w-full !h-full min-h-0"
-                className="w-full h-full border-none"
-                src='/images/nft.webp'
-                displayName={moduleSettings.displayNftName}
-                displayPrice={moduleSettings.displayNftPrice} />
-
-        </ModuleCard><Modal
-            title="Change avatar"
-            description="Select an NFT from your wallet to be displayed."
-            showModal={showModal}
-            closeModal={closeModal}
-        >
-                <div className="relative mb-4 md:mb-8">
-                    <div ref={sliderRef} className="keen-slider ">
-                        {/* Put array map here */}
-                    </div>
-                    <div className="absolute top-1/2 left-0 transform -translate-y-1/2  flex flex-row justify-between">
-                        {loaded && instanceRef.current && (
-                            <>
-                                <SliderButton
-                                    left
-                                    onClick={(e) => e.stopPropagation() || instanceRef.current?.prev()}
-                                    disabled={currentSlide === 0} />
-                            </>
-                        )}
-                    </div>
-                    <div className="absolute top-1/2 right-0 transform -translate-y-1/2  flex flex-row justify-between">
-                        {loaded && instanceRef.current && (
-                            <>
-                                <SliderButton
-                                    right
-                                    onClick={(e) => e.stopPropagation() || instanceRef.current?.next()}
-                                    disabled={currentSlide === instanceRef.current.track.details.slides.length - 3} />
-                            </>
-                        )}
-                    </div>
-                </div>
-                <div className="flex justify-end">
-                    <Button className="bg-white !text-[#1A1921]" onClick={closeAvatarModal}>
-                        Select
-                    </Button>
-                </div>
-            </Modal></>
-    );
+      <Modal
+        title="Change avatar"
+        description="Select an NFT from your wallet to be displayed."
+        showModal={showModal}
+        closeModal={closeModal}
+      >
+        <div className="relative mb-4 md:mb-8">
+          <div ref={sliderRef} className="keen-slider ">
+            {/* Put array map here */}
+          </div>
+          <div className="absolute top-1/2 left-0 transform -translate-y-1/2  flex flex-row justify-between">
+            {loaded && instanceRef.current && (
+              <>
+                <SliderButton
+                  left
+                  onClick={(e) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                  disabled={currentSlide === 0}
+                />
+              </>
+            )}
+          </div>
+          <div className="absolute top-1/2 right-0 transform -translate-y-1/2  flex flex-row justify-between">
+            {loaded && instanceRef.current && (
+              <>
+                <SliderButton
+                  right
+                  onClick={(e) =>
+                    e.stopPropagation() || instanceRef.current?.next()
+                  }
+                  disabled={
+                    currentSlide === instanceRef.current.slides.length - 3
+                  }
+                />
+              </>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-end">
+          <Button
+            className="bg-white !text-[#1A1921]"
+            onClick={closeModal}
+          >
+            Select
+          </Button>
+        </div>
+      </Modal>
+    </>
+  );
 };
 
-
-
 export default SingleNft;
-
-
